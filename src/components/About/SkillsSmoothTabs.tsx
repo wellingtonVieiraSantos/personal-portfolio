f9b9eba32b04b38660ced7eed8d6ecf92e6a1602
Wellington Vieira <wtvsantos@gmail.com>
2025-03-28 20:07:06 -0300

    style: change skills layout

--- src/components/About/SkillsSmoothTabs.tsx
+++ src/components/About/SkillsSmoothTabs.tsx
@@ -15,30 +15,30 @@ export default function SkillSmoothTabs() {
         transition: { delay: 0.2, duration: 0.4 }
       }}
       viewport={{ once: true }}
-      className='w-full max-w-2xl bg-primary-bg dark:bg-primary-bg-dark rounded m-auto grid grid-flow-row px-2 py-4 overflow-hidden shadow'
+      className='w-full max-w-4xl bg-primary-bg dark:bg-primary-bg-dark lg:rounded m-auto grid grid-flow-row p-4 overflow-hidden shadow'
     >
       <AnimatePresence mode='wait'>
         <motion.div
-          className='h-[400px] mb-4 grid'
+          className='mb-4 grid grid-flow-col gap-4'
           key={active}
           initial={{ opacity: 0, x: '50%' }}
           animate={{ opacity: 1, x: 0 }}
           exit={{ opacity: 0, x: '-50%' }}
           transition={{ delay: 0.1, duration: 0.3 }}
         >
-          <div className='flex justify-evenly items-center'>
-            <Image
-              src={skills.find(skill => skill.id === active)?.img}
-              alt={skills.find(skill => skill.id === active)?.title + ' logo'}
-              className='w-24 h-24 aspect-square border border-secondary-text dark:bg-white p-2 rounded'
-            />
-            <h3 className='text-3xl font-montserrat-title'>
+          <Image
+            src={skills.find(skill => skill.id === active)?.img}
+            alt={skills.find(skill => skill.id === active)?.title + ' logo'}
+            className='h-16 w-auto border border-secondary-text dark:bg-white p-2 rounded'
+          />
+          <div className='grid place-items-start gap-2'>
+            <h3 className='text-2xl font-montserrat-title'>
               {skills.find(skill => skill.id === active)?.title}
             </h3>
+            <p className=' m-auto text-justify text-lg '>
+              {skills.find(skill => skill.id === active)?.content}
+            </p>
           </div>
-          <p className='max-w-[400px] m-auto text-justify text-lg'>
-            {skills.find(skill => skill.id === active)?.content}
-          </p>
         </motion.div>
       </AnimatePresence>
 
@@ -47,12 +47,12 @@ export default function SkillSmoothTabs() {
           <button
             key={skill.id}
             onClick={() => setActive(skill.id)}
-            className={`py-2 px-3 flex flex-1 justify-center relative border border-secondary-bg dark:border-secondary-bg-dark rounded ${
+            className={`py-2 px-4 flex flex-1 justify-center relative border border-secondary-bg dark:border-secondary-bg-dark rounded ${
               active === skill.id &&
               'text-button-text dark:text-secondary-bg-dark'
             }  font-montserrat-title`}
           >
-            <li className='z-10'>{skill.title}</li>
+            <li className='z-10 '>{skill.title}</li>
             {active === skill.id && (
               <motion.div
                 layoutId='background-button'
--- src/utils/skillsData.ts
+++ src/utils/skillsData.ts
@@ -20,104 +20,104 @@ export const skills = [
     title: 'HTML',
     img: html,
     content:
-      'HTML (HyperText Markup Language) é a linguagem de marcação usada para estruturar páginas da web. Ele define a organização do conteúdo por meio de elementos como títulos, parágrafos, imagens, links e listas.'
+      'HTML (HyperText Markup Language) é a linguagem de marcação usada para estruturar páginas da web.'
   },
   {
     id: 2,
     title: 'CSS',
     img: css,
     content:
-      'CSS (Cascading Style Sheets) é a linguagem usada para estilizar páginas da web, definindo cores, fontes, espaçamentos, layouts e animações. Ele permite separar o conteúdo (HTML) da apresentação visual, garantindo flexibilidade e melhor manutenção do design. Com CSS, é possível criar interfaces responsivas e atraentes para diferentes dispositivos.'
+      'CSS (Cascading Style Sheets) é a linguagem usada para estilizar páginas da web, definindo cores, fontes, espaçamentos, layouts e animações.'
   },
   {
     id: 3,
     title: 'JavaScript',
     img: js,
     content:
-      'JavaScript é uma linguagem de programação voltada para a criação de interatividade em páginas da web. Ele permite manipular o HTML e o CSS dinamicamente, responder a eventos do usuário, validar formulários, criar animações e desenvolver aplicações web completas. '
+      'JavaScript é uma linguagem de programação voltada para a criação de interatividade em páginas da web.'
   },
   {
     id: 4,
     title: 'React',
     img: react,
     content:
-      'React é uma biblioteca JavaScript para a criação de interfaces de usuário interativas e eficientes. Ele utiliza um modelo baseado em componentes reutilizáveis e um estado gerenciado de forma reativa, permitindo a construção de aplicações web dinâmicas e performáticas. Com o uso do Virtual DOM, o React otimiza a atualização da interface, tornando as interações mais rápidas. '
+      'React é uma biblioteca JavaScript para a criação de interfaces de usuário interativas e eficientes.'
   },
   {
     id: 5,
     title: 'Next.js',
     img: next,
     content:
-      'Next.js é um framework baseado em React que facilita o desenvolvimento de aplicações web fullstack. Ele oferece recursos avançados, como renderização híbrida (SSR e SSG), otimização de performance, roteamento simplificado e suporte nativo a API Routes. '
+      'Next.js é um framework baseado em React que facilita o desenvolvimento de aplicações web fullstack.'
   },
   {
     id: 6,
     title: 'TypeScript',
     img: ts,
     content:
-      'TypeScript (TS) é uma linguagem de programação que é uma superset do JavaScript, adicionando tipagem estática e recursos de orientação a objetos. Ele permite detectar erros de tipo durante o desenvolvimento, antes da execução do código, aumentando a confiabilidade e a manutenção de grandes projetos.'
+      'TypeScript (TS) é uma linguagem de programação que é uma superset do JavaScript, adicionando tipagem estática e recursos de orientação a objetos.'
   },
   {
     id: 7,
     title: 'Tailwind',
     img: tailwind,
     content:
-      'Tailwind CSS é um framework de CSS utilitário que permite criar designs personalizados diretamente no HTML, utilizando classes pré-definidas para estilizar elementos. Em vez de escrever estilos CSS personalizados, o Tailwind oferece uma abordagem mais prática e flexível, onde você pode combinar suas classes para controlar rapidamente a aparência de um site. '
+      'Tailwind CSS é um framework de CSS utilitário que permite criar designs personalizados diretamente no HTML, utilizando classes pré-definidas para estilizar elementos.'
   },
   {
     id: 8,
     title: 'Styled',
     img: styled,
     content:
-      'Styled Components é uma biblioteca para React e outras bibliotecas JavaScript que permite criar componentes de estilo com CSS dentro do JavaScript. Ele utiliza o conceito de CSS-in-JS, onde você escreve estilos diretamente no código de componentes usando template literals.'
+      'Styled Components é uma biblioteca para React e outras bibliotecas JavaScript que permite criar componentes de estilo com CSS dentro do JavaScript.'
   },
   {
     id: 9,
     title: 'Motion',
     img: motion,
     content:
-      'Framer Motion é uma biblioteca de animações para React que permite criar animações complexas de forma simples e declarativa. Ela oferece um conjunto de APIs poderosas para animar elementos, como transições, gestos e interações, com controle total sobre os parâmetros de tempo, easing e comportamento das animações.'
+      'Framer Motion é uma biblioteca de animações para React que permite criar animações complexas de forma simples e declarativa.'
   },
   {
     id: 10,
     title: 'Node',
     img: node,
     content:
-      'Node.js é um ambiente de execução JavaScript do lado do servidor, baseado no motor V8 do Google Chrome. Ele permite que você execute JavaScript fora do navegador, o que o torna ideal para criar servidores web e aplicações backend escaláveis e de alto desempenho.'
+      'Node.js é um ambiente de execução JavaScript do lado do servidor, baseado no motor V8 do Google Chrome.'
   },
   {
     id: 11,
     title: 'Express',
     img: express,
     content:
-      'Express.js é um framework minimalista e flexível para Node.js, usado para criar aplicações web e APIs. Ele facilita o roteamento de requisições HTTP, o gerenciamento de middlewares e a integração com bancos de dados, simplificando o processo de desenvolvimento backend.'
+      'Express.js é um framework minimalista e flexível para Node.js, usado para criar aplicações web e APIs.'
   },
   {
     id: 12,
     title: 'MySQL',
     img: mysql,
     content:
-      'MySQL é um sistema de gerenciamento de banco de dados relacional (RDBMS) amplamente utilizado, baseado no modelo de dados estruturado em tabelas. Ele usa SQL (Structured Query Language) para criar, manipular e consultar os dados. '
+      'MySQL é um sistema de gerenciamento de banco de dados relacional (RDBMS) amplamente utilizado, baseado no modelo de dados estruturado em tabelas.'
   },
   {
     id: 13,
     title: 'MongoDB',
     img: mongodb,
     content:
-      'MongoDB é um banco de dados NoSQL orientado a documentos, que armazena dados em formato BSON (uma extensão binária do JSON). Ele é altamente escalável e flexível, permitindo o armazenamento de dados não estruturados ou semi-estruturados.'
+      'MongoDB é um banco de dados NoSQL orientado a documentos, que armazena dados em formato BSON (uma extensão binária do JSON).'
   },
   {
     id: 14,
     title: 'Git',
     img: git,
     content:
-      'Git é um sistema de controle de versão distribuído, utilizado para gerenciar e acompanhar as mudanças no código-fonte ao longo do tempo. Ele permite que múltiplos desenvolvedores trabalhem de forma colaborativa em um projeto, facilitando o controle das alterações feitas por diferentes pessoas.'
+      'Git é um sistema de controle de versão distribuído, utilizado para gerenciar e acompanhar as mudanças no código-fonte ao longo do tempo.'
   },
   {
     id: 15,
     title: 'Github',
     img: github,
     content:
-      'GitHub é uma plataforma de hospedagem de código-fonte baseada no Git, que facilita o versionamento e colaboração em projetos de software. Ele oferece funcionalidades como repositórios privados e públicos, controle de versão, pull requests, issues e integração com outras ferramentas de desenvolvimento. '
+      'GitHub é uma plataforma de hospedagem de código-fonte baseada no Git, que facilita o versionamento e colaboração em projetos de software.'
   }
 ]
